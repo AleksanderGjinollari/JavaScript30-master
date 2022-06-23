d6a7a63f48000e4ed2801b847961d310ff01232c
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-23 09:52:26 +0200

    creating a timer

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,23 @@
+let countdown;
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it
+    if (secondsLeft <= 0) {
+      console.log(0);
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = seconds / 60;
+  console.log(seconds);
+}
